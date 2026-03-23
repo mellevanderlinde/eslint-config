cdf67fe180d62cb70949fbe04a007e4bc8887db3
Melle van der Linde <118454433+mellevanderlinde@users.noreply.github.com>
2026-03-23 22:06:31 +0100

    feat: first version of config (#1)
    * chore: init antfu
    
    * feat: defineConfig
    
    * feat: disable no-new
    
    * feat: more rules
    
    * chore: TS build
    
    * feat: test padding

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import { antfu } from '@antfu/eslint-config'
+
+export function defineConfig(): ReturnType<typeof antfu> {
+  return antfu({
+    rules: {
+      'curly': 'error',
+      'no-new': 'off',
+      'perfectionist/sort-objects': 'error',
+      'test/padding-around-all': 'error',
+      'ts/explicit-function-return-type': 'error',
+      'unicorn/no-empty-file': 'error',
+    },
+    typescript: true,
+  })
+}
