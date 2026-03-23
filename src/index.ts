27dcf8ecf6a8b8e1d7e7b1bef99e16f5788812e9
Melle <118454433+mellevanderlinde@users.noreply.github.com>
2026-03-23 21:47:59 +0100

    feat: more rules

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,13 @@ import { antfu } from '@antfu/eslint-config'
 
 export function defineConfig(): ReturnType<typeof antfu> {
   return antfu({
-    typescript: true,
     rules: {
+      'curly': 'error',
       'no-new': 'off',
+      'perfectionist/sort-objects': 'error',
+      'ts/explicit-function-return-type': 'error',
+      'unicorn/no-empty-file': 'error',
     },
+    typescript: true,
   })
 }
