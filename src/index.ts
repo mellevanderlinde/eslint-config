c8cb2dd1d94609d485e7a1632c29a3c52d586f8a
Melle <118454433+mellevanderlinde@users.noreply.github.com>
2026-03-23 22:16:34 +0100

    feat: consistent objects

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,8 @@ export function defineConfig(): ReturnType<typeof antfu> {
       'perfectionist/sort-objects': 'error',
       'style/array-bracket-newline': ['error', 'consistent'],
       'style/array-element-newline': ['error', 'consistent'],
+      'style/object-curly-newline': ['error'],
+      'style/object-property-newline': ['error', { allowAllPropertiesOnSameLine: true }],
       'test/padding-around-all': 'error',
       'ts/explicit-function-return-type': 'error',
       'unicorn/no-empty-file': 'error',
