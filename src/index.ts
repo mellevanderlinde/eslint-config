813ec23f4e2848c3e7c182a2a5870d1109113ee4
Melle van der Linde <118454433+mellevanderlinde@users.noreply.github.com>
2026-03-24 15:48:35 +0100

    feat: add more rules (#2)
    * feat: consistent arrays
    
    * feat: consistent objects
    
    * feat: no non null assertions
    
    * chore: add todo
    
    * feat: more rules
    
    * feat: no blank lines between imports

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,18 @@ export function defineConfig(): ReturnType<typeof antfu> {
   return antfu({
     rules: {
       'curly': 'error',
+      'no-else-return': 'error',
+      'no-lonely-if': 'error',
       'no-new': 'off',
+      'padding-line-between-statements': ['error', { blankLine: 'never', next: 'import', prev: 'import' }],
       'perfectionist/sort-objects': 'error',
+      'style/array-bracket-newline': ['error', 'consistent'],
+      'style/array-element-newline': ['error', 'consistent'],
+      'style/object-curly-newline': ['error'],
+      'style/object-property-newline': ['error', { allowAllPropertiesOnSameLine: true }],
       'test/padding-around-all': 'error',
       'ts/explicit-function-return-type': 'error',
+      'ts/no-non-null-assertion': 'error',
       'unicorn/no-empty-file': 'error',
     },
     typescript: true,
